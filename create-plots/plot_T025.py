fafcebee7abe61404a7097d58382ae830eace2c9
Jack <jack.d.holden@gmail.com>
2022-08-24 09:57:57 +0100

    T=0.25 plot can use more data also changed colours and some logic in T=0.25 the plot with 1/N^2 on the x axis was redundant and was actually fitting too many points so now the data is shared between both points

--- create-plots/plot_T025.py
+++ create-plots/plot_T025.py
@@ -111,8 +111,10 @@ def take_large_N_unconstrained(ns, WLs, WL_errs):
     WLs = np.array(WLs)
     WL_errs = np.array(WL_errs)
     (m, c), cov = np.polyfit(1/ns/ns, WLs, deg=1, w=1/WL_errs, cov='unscaled')
-    cerr = np.sqrt(np.diag(cov))[1]
-    return c, cerr
+    merr, cerr = np.sqrt(np.diag(cov))
+    largeN_wl = gvar.gvar(c, cerr)
+    grad = gvar.gvar(m, merr)
+    return largeN_wl, grad
 
 def plot_T025():
     # ** COLLECT POINTS WE WILL USE **
@@ -120,8 +122,8 @@ def plot_T025():
     CONSTANT_NT = 24
     T = 0.25
     WL_TYPE = "dec"
-    # Ns = (16, 24, 32, 48, 64, 96, 128)
-    Ns = (48, 64, 96, 128)
+    Ns = (16, 24, 32, 48, 64, 96, 128)
+    # Ns = (48, 64, 96, 128)
     points = {}
     for n in Ns:
         points[n] = Point(n, n, UNCONSTRAINED_P_FILLER, T, CONSTANT_NT, WL_TYPE)
@@ -133,10 +135,12 @@ def plot_T025():
 
     # Large N extrapolation
     ## " For the extrapolation to N = ∞, we used data points at N = 24, 32, 48, 64, 96, 128 for L = 1, 2, 3, 4 and N = 48, 64, 96, 128 for L = 5. "
-    largeN = np.zeros(5)
-    largeN_errs = np.zeros(5)
+    largeN_wl = []
+    largeN_extrap_grad = []
+    largeN_by_L = {}
+    largeN_extrap_grad_by_L = {}
     Ls = (1,2,3,4,5)
-    low_Ns = (48, 64, 96, 128)
+    low_Ns = (24, 32, 48, 64, 96, 128)
     high_Ns = (48, 64, 96, 128)
     for l in Ls:
         if l<5:
@@ -145,7 +149,11 @@ def plot_T025():
             use_Ns = high_Ns
         WLs = [points[n].WL_data_av[l-1] for n in use_Ns]
         errs = [points[n].WL_errs[l-1] for n in use_Ns]
-        largeN[l-1], largeN_errs[l-1] = take_large_N_unconstrained(use_Ns, WLs, errs)
+        lNwl, grad = take_large_N_unconstrained(use_Ns, WLs, errs)
+        largeN_wl.append(lNwl)
+        largeN_extrap_grad.append(grad)
+        largeN_by_L[l] = lNwl
+        largeN_extrap_grad_by_L[l] = grad
         
     for n in Ns:
         ws, err = points[n].WL_data_av, points[n].WL_errs
@@ -154,9 +162,9 @@ def plot_T025():
         ax.scatter(ls, ws, label=f"$N={n_label}$", marker=p_common.MARKERS_BY_N[n], s=70, linewidths=1, c=p_common.COLOURS_BY_N[n])
         ax.errorbar(ls, ws, yerr=err, linestyle='none', capsize=4, linewidth=2, c=p_common.COLOURS_BY_N[n])
     
-    ax.scatter(Ls, largeN, label=f"$N=\\infty$", marker=p_common.MARKERS_BY_N['large'], s=70, linewidths=1, c=p_common.COLOURS_BY_N['large'])
-    ax.errorbar(Ls, largeN, yerr=largeN_errs, linestyle='none', capsize=4, linewidth=2, c=p_common.COLOURS_BY_N['large'])
-    yy = np.log(gvar.gvar(largeN, largeN_errs))
+    ax.scatter(Ls, gvar.mean(largeN_wl), label=f"$N=\\infty$", marker=p_common.MARKERS_BY_N['large'], s=70, linewidths=1, c=p_common.COLOURS_BY_N['large'])
+    ax.errorbar(Ls, gvar.mean(largeN_wl), yerr=gvar.sdev(largeN_wl), linestyle='none', capsize=4, linewidth=2, c=p_common.COLOURS_BY_N['large'])
+    yy = np.log(largeN_wl)
     (m, c), cov = np.polyfit(Ls, gvar.mean(yy), w=1/gvar.sdev(yy), deg=1, cov='unscaled')
     merr, cerr = np.sqrt(np.diag(cov))
     grad = gvar.gvar(m, merr)
@@ -167,7 +175,7 @@ def plot_T025():
     ax.plot(xlim, gvar.mean(yfit), c=p_common.COLOURS_BY_N['large'])
 
     predicted_y = np.exp(-1.0/2.0/0.25*xlim)
-    ax.plot(xlim, predicted_y, color='blue')
+    ax.plot(xlim, predicted_y, color='red')
 
     ax.set_xlabel("$L$", fontsize=20)
     ax.set_ylabel("$W(L)/N$", fontsize=20)
@@ -190,10 +198,12 @@ def plot_T025():
     # intcpt = {}
     for l in show_Ls:
         xx, wl, wl_errs = zip(*[(1/pt.n/pt.n, pt.WL_data_av[l-1], pt.WL_errs[l-1]) for pt in points.values()])
-        (m, c), cov = np.polyfit(xx, wl, deg=1, w=1/np.array(wl_errs), cov='unscaled')
-        merr, cerr = np.sqrt(np.diag(cov))
-        grad = gvar.gvar(m, merr)
-        intcpt = gvar.gvar(c, cerr)
+        # (m, c), cov = np.polyfit(xx, wl, deg=1, w=1/np.array(wl_errs), cov='unscaled')
+        # merr, cerr = np.sqrt(np.diag(cov))
+        # grad = gvar.gvar(m, merr)
+        # intcpt = gvar.gvar(c, cerr)
+        grad = largeN_extrap_grad_by_L[l]
+        intcpt = largeN_by_L[l]
         
         ax.scatter(xx, wl, label=f"$L={l}$", marker=p_common.MARKERS_BY_L[l], s=70, linewidths=1, c=p_common.COLOURS_BY_L[l])
         ax.errorbar(xx, wl, yerr=wl_errs, linestyle='none', capsize=4, linewidth=2, c=p_common.COLOURS_BY_L[l])
@@ -228,3 +238,10 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+# TODO:
+# 1) change so that only do interpolation once; use results of grad and intercept in same plot
+# 2) get rid of surplus code here
+# 3) ... why do decays not match well??
+# 4) output the numbers
